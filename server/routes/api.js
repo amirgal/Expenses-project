d2b60a127d6f0150bc1ce0776b2729ad4408a668
amirgal <amirc538@gmail.com>
2020-02-12 16:14:11 +0200

    initial commit

---
+++ server/routes/api.js
@@ -0,0 +1,8 @@
+  
+const express = require('express')
+const router = express.Router()
+const request = require('request')   //if we need to request from external api
+
+
+
+module.exports = router
