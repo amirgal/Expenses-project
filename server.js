6ee1439d79b9aac04dd56e8f762515764058cc50
amirgal <amirc538@gmail.com>
2020-02-12 19:01:57 +0200

    expenses project

--- server.js
+++ server.js
@@ -1,15 +1,27 @@
 const express = require('express')
 const path = require('path')
+var bodyParser = require('body-parser')
 const api = require('./server/routes/api')
 const app = express()
-
+const mongoose = require('mongoose')
+mongoose.connect("mongodb://localhost/expenses", { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify:false })
 
 app.use(express.static(path.join(__dirname, 'dist')))
 app.use(express.static(path.join(__dirname, 'node_modules')))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 app.use('/', api)
 
-const port = 8080
+// //Run Once to load data
+// const expensesData = require('./expenses.json')
+// const Expense = require('./server/models/Expense')
+// expensesData.forEach(exp => {
+//     const e = new Expense(exp)
+//     e.save()
+// });
+
+const port = 3000
 app.listen(port, function () {
     console.log(`Running server on port ${port}`)
 })
--- server/routes/api.js
+++ server/routes/api.js
@@ -1,8 +1,57 @@
-  
 const express = require('express')
 const router = express.Router()
-const request = require('request')   //if we need to request from external api
+const moment = require('moment')
+const Expense = require('../models/Expense')
 
+router.get('/expenses', function(req,res) {
+    Expense.find({}).sort({date:-1}).exec(function(err,expenses){
+        res.send(expenses)
+    })
+})
+
+//Need to add a promise then log the item and price 
+router.post('/new',function(req,res) {
+    const exp = req.body
+    const newExp = new Expense({item:exp.item, amount: exp.amount, group: exp.group, date: exp.date ? moment(exp.date).format('LLLL') : moment().format('LLLL')})
+    const expPromise = newExp.save()
+    expPromise.then(function(){
+        console.log(`spent ${exp.amount} dollars on ${exp.item}`)
+    })
+    res.end()
+})
+
+router.put('/update/:group1/:group2', function(req,res) {
+    const {group1, group2} = req.params
+    Expense.findOneAndUpdate({group:group1},{group:group2},{new: true}).exec(function(err,result) {
+        res.send(`changed ${result.item} group from ${group1} to ${group2}`)
+    })
+})
+
+router.get('/expenses/:group', function(req,res) {
+    const total = req.query.total
+    const group = req.params.group
+    if(total !== 'true'){
+        Expense.find({group}).exec(function(err,expenses) {
+            res.send(expenses)
+        })
+    } else {
+        Expense.aggregate([
+            {$match: {group}},
+            {$group:
+                {
+                    _id: "$group",
+                    totalAmount: {$sum: "$amount"}
+                }
+            }
+        ]).exec(function(err,total) {
+            res.send(total)
+        })
+    }
+})
+
+router.get('/expenses', function(req,res) {
+
+})
 
 
 module.exports = router
